78d4eb0f3d81a0c2bd12d3929a46c19123f09c9d
glollier <lollier.guillaume@gmail.com>
2018-12-17 13:00:00 +0100

    Added main  + cleared some files

---
+++ splineRacer/TP_Main/CMakeFiles/TP_Main_Main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP_Main_Main.dir/Main.cpp.o"
+  "TP_Main_Main.pdb"
+  "TP_Main_Main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP_Main_Main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ splineRacer/TP_Main/CMakeFiles/TP_template_SDLtemplate.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TP_template_SDLtemplate.dir/SDLtemplate.cpp.o"
+  "TP_template_SDLtemplate.pdb"
+  "TP_template_SDLtemplate"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TP_template_SDLtemplate.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

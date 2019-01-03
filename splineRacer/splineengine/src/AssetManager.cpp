21037e41cc54f987c5574e70c2d89aef670202e1
torresf <torres.florian1@gmail.com>
2019-01-03 14:54:35 +0100

    New Skybox

--- splineRacer/splineengine/src/AssetManager.cpp
+++ splineRacer/splineengine/src/AssetManager.cpp
@@ -15,7 +15,7 @@ void AssetManager::loadAssets() {
 	_models.insert(std::make_pair("plane", 			Model("plane",		"planetexture2.jpg")));
 	_models.insert(std::make_pair("wheelbarrow",	Model("wheelbarrow","planetexture2.jpg")));
 	_models.insert(std::make_pair("singe", 			Model("singe",		"planetexture.jpg")));
-	_models.insert(std::make_pair("skybox", 		Model("skybox",		"nuages.png")));
+	_models.insert(std::make_pair("skybox", 		Model("skybox",		"skybox_desert.png")));
 	_models.insert(std::make_pair("cloud", 			Model("cloud",		"cloud.jpg")));
 	_models.insert(std::make_pair("coin", 			Model("coin",		"coin.png")));
 	_models.insert(std::make_pair("prism", 			Model("prism",		"cloud.jpg")));
